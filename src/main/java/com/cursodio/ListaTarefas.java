c9894ddaa267c572765d4da46ecfd5b7bea419b4
Gabriel Lima <gabriel1277lima@gmail.com>
2023-08-12 14:08:09 -0300

    first commit

---
+++ src/main/java/com/cursodio/ListaTarefas.java
@@ -0,0 +1,50 @@
+package com.cursodio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    private List<Tarefas> tarefaList;
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefas(descricao));
+    }
+
+    public void removerTarefa(String decricao) {
+        List<Tarefas> tarefasRemover = new ArrayList<>();
+        for (Tarefas t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(decricao)) {
+                tarefasRemover.add(t);
+            }
+
+        }
+        tarefaList.removeAll(tarefasRemover);
+    }
+
+    public int obterNumeroTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricao() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefa = new ListaTarefas();
+        System.out.println("O numero total é: " + listaTarefa.obterNumeroTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        listaTarefa.obterDescricao();
+        System.out.println("O numero total é: " + listaTarefa.obterNumeroTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O numero total é: " + listaTarefa.obterNumeroTarefas());
+
+    }
+}
---
+++ src/main/java/com/cursodio/Tarefas.java
@@ -0,0 +1,20 @@
+package com.cursodio;
+
+public class Tarefas {
+    // atributo
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Tarefas(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefas [descricao=" + descricao + "]";
+    }
+
+}
