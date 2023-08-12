1a43d0652f1a5b6de719d71a211d7987af4f5712
Gabriel Lima <gabriel1277lima@gmail.com>
2023-08-12 17:34:42 -0300

    .

---
+++ src/main/java/com/cursodio/Convidado/ConjuntoConvidados.java
@@ -0,0 +1,47 @@
+package com.cursodio.Convidado;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoConvite(int codigoConvite) {
+        Convidado convidadoRemover = null;
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoRemover = c;
+            }
+            break;
+        }
+        convidadoSet.remove(convidadoRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set.");
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set.");
+
+    }
+
+}
